433a85f40e51f9eb913a5747654219d3a9eac7bd
Caio Henrique Andrade Lima <mac@MacBook-Pro-de-Macbook-2.local>
2022-04-21 13:22:47 -0300

    comeca session

--- spring-rest-sample-master/src/main/java/com/techsystem/request/ManagerRequest.kt
+++ spring-rest-sample-master/src/main/java/com/techsystem/request/ManagerRequest.kt
@@ -4,6 +4,8 @@ import com.techsystem.extension.gson
 import com.techsystem.extension.toJson
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 
 inline fun <reified T, Y> managerRequest(json: String?, call: (it: T?) -> Pair<ResponseEntity.BodyBuilder, Y?>): ResponseEntity<*> {
@@ -36,11 +38,41 @@ inline fun <reified T, Y> managerRequest(json: String?, call: (it: T?) -> Pair<R
 fun provideSession() = null
 
 
-fun provideUserAuthenticate(): UserCassandraModel? {
+fun provideUserAuthenticate(): UserSessionModel? {
 
     if(SecurityContextHolder.getContext().authentication != null) {
-        return (SecurityContextHolder.getContext().authentication as AuthenticationToken).authenticatedUser.user.user;
+        return (SecurityContextHolder.getContext().authentication as UserSessionModel)
     }
 
     return null;
+}
+
+class UserSessionModel() : Authentication {
+    override fun getName(): String {
+
+    }
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCredentials(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDetails(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPrincipal(): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun isAuthenticated(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun setAuthenticated(isAuthenticated: Boolean) {
+        TODO("Not yet implemented")
+    }
 }
